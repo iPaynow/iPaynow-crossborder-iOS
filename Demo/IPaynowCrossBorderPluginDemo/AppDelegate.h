bd3fcaa5b9ce569705207a73e32ac0e75de68667
Hstripe <hrbistu@gmail.com>
2017-04-13 10:49:09 +0800

    初始上线版本

---
+++ Demo/IPaynowCrossBorderPluginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPaynowCrossBorderPluginDemo
+//
+//  Created by 黄睿 on 2017/1/5.
+//  Copyright © 2017年 ipaynow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/IPaynowCrossBorderPluginDemo/CrossBorderViewController.h
@@ -0,0 +1,13 @@
+//
+//  CrossBorderViewController.h
+//  IPaynowCrossBorderPluginDemo
+//
+//  Created by 黄睿 on 2017/1/5.
+//  Copyright © 2017年 ipaynow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrossBorderViewController : UIViewController
+
+@end
